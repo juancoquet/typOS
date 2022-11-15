6d6806cbf1fd101c9f9ac164068532b947357989
juancoquet <juancoquet@gmail.com>
2022-11-15 01:28:13 +0000

    fix comment typos. typOS name already paying off

--- boot_sect.asm
+++ boot_sect.asm
@@ -5,12 +5,12 @@
 loop:                   ; define a label, 'loop', that will allow
                         ; us to jump back to it, forever.
     
-    jmp loop            ; use simple CPU instruction that jump
+    jmp loop            ; use simple CPU instruction that jumps
                         ; to a new memory address to continue execution.
                         ; in this case, jump to the address of the current
                         ; instruction.
 
-times 510-($-$$) db 0   ; when compiled, the program must fin into 512 bytes,
+times 510-($-$$) db 0   ; when compiled, the program must fit into 512 bytes,
                         ; with the last two bytes being the magic number 0xaa55
                         ; that lets BIOS know this is the boot sector. this code
                         ; tells the assembly compiler to pad out the program with
